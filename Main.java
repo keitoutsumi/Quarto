215918b18722ec42ba79287106fc44b1f94d07ef
keitoutsumi <90272383+keitoutsumi@users.noreply.github.com>
2023-07-09 23:50:00 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,18 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Game game;
 
-        // Gameクラスの初期化
         if (args.length == 2) {
-            // コマンドライン引数でアドレスとポートを指定する
             String address_name = args[0];
             int port_num = Integer.parseInt(args[1]);
             game = new Game(address_name, port_num);
         } else if (args.length == 1) {
-            // コマンドライン引数でポートを指定する
             int port_num = Integer.parseInt(args[0]);
             game = new Game(port_num);
         } else if (args.length == 0) {
-            // コマンドライン引数がない場合はデフォルト設定を使う
             game = new Game();
         } else {
             System.out.println("Error: Too many arguments");
